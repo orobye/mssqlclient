8245b3f479ee8bbe616b298360d87ff61f2c1446
12hours <mon-keigh@ya.ru>
2017-06-22 12:04:29 +0000

    connection to server

--- src/main/java/by/bockroir/CmdClient.java
+++ src/main/java/by/bockroir/CmdClient.java
@@ -1,11 +1,31 @@
 package by.bockroir;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class CmdClient {
     private Connection conn;
     
     public void setConnection(Connection conn){
         this.conn = conn;
+    }
+    
+    public void runClient() throws SQLException{
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        
+        while (!command.equals("exit")){
+            Statement stmt = conn.createStatement();
+            stmt.execute(command);
+            ResultSet rs = stmt.getResultSet();
+            if (rs != null){
+                while (rs.next()) {
+                    System.out.println(rs.getString(1));
+                }
+            }
+        }
     }
 }
--- src/main/java/by/bockroir/Main.java
+++ src/main/java/by/bockroir/Main.java
@@ -1,10 +1,25 @@
 package by.bockroir;
 
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+    
     public static void main(String[] args) {
-        try (Connector connector = new Connector("192.168.1.4", "1433", "CKRO", "polystat", "8095461");){
+        if (args.length < 5){
+            System.out.println("pass args: HOST PORT DATABASE_NAME USERNAME PASSWORD");
+            return;        
+            }
+        
+        try (Connector connector = new Connector(args[0], args[1], args[2], args[3], args[4]);){
             CmdClient cmdClient = new CmdClient();
             cmdClient.setConnection(connector.getConnection());
+            logger.info("Connection OK");
+            cmdClient.runClient();
+        } catch (SQLException e){
+            e.printStackTrace();
+            return;
         }
     }
 }
